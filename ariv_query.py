fbd6766f841fcab7bbf4cc26d17af5cd8243430d
AliAlkadhim <aa18dg@my.fsu.edu>
2023-07-15 12:14:34 +0200

    start

---
+++ ariv_query.py
@@ -0,0 +1,8 @@
+import urllib
+import feedparser
+import urllib.request as libreq
+print(help(libreq))
+
+# with libreq.urlopen('http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1') as url:
+#     r = url.read()
+# print(r)
